65d882a2e248d13542d68374876752ed6a4d19fb
omair-arshad-yousaf <omair.aarshad2000@gmail.com>
2022-07-18 12:07:53 +0500

    updated

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const connectDB= async()=>{
         mongoose.connect('mongodb://localhost:27017/Web_Final',{
     useNewUrlParser: true,
 })
-    console.log(`connection successful`);
+    console.log(`connection successful at port 5500`);
     }
 catch(e){
     console.log(`no connection`);
